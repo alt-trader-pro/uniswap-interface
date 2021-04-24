76550efbef44f59ee35aceeafadc00bec99e1ee8
Jeremy Hanlon <50811732+kitchenbeats@users.noreply.github.com>
2021-04-24 15:55:53 -0700

    Update index.ts Router Address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x75adC4F3aD70d439074692c7D7Cf34Fe6809Bba1'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
