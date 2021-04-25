89949786afb1c583270756acd1561d2edd884b10
Jeremy Hanlon <50811732+kitchenbeats@users.noreply.github.com>
2021-04-24 18:51:11 -0700

    Update connectors to default

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -13,7 +13,7 @@ const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
 const PORTIS_ID = process.env.REACT_APP_PORTIS_ID
 const WALLETCONNECT_BRIDGE_URL = process.env.REACT_APP_WALLETCONNECT_BRIDGE_URL
 
-export const NETWORK_CHAIN_ID: number = parseInt(CHAIN_ID ?? '1')
+export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '1')
 
 if (typeof NETWORK_URL === 'undefined') {
   throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
